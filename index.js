8d7a13dcb06725ee4c098de55f9f05b0f25573f2
nomachi <nomachi.masahiro@gmail.com>
2023-10-08 19:52:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('自動デプロイだよ!!')
+  res.send('main の内容変えたら自動で deploy 出来るらしいのでテスト')
 })
  
 app.listen(80)
